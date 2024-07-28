87f599135c7e42f09acf1b6d646394d861b0471d
hvasi001 <haripriya.vasireddy@gmail.com>
2024-07-28 10:29:23 -0700

    hari

--- AlienInvasionGame/src/alien_invasion.py
+++ AlienInvasionGame/src/alien_invasion.py
@@ -7,6 +7,7 @@ from game_stats import GameStats
 from ship import Ship
 import game_functions as gf
 from alien import Alien
+from button import Button
 
 
 def run_game():
@@ -16,6 +17,9 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    # Make the Play button.
+    play_button = Button(ai_settings, screen, "Play")
 
     # Create an instance to store game statistics.
     stats = GameStats(ai_settings)
@@ -32,29 +36,36 @@ def run_game():
     # Make an alien.
     alien = Alien(ai_settings, screen)
 
+
     # Start the main loop for the game.
     while True:
-        gf.check_events(ai_settings, screen, ship, bullets)
+        gf.check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
 
         if stats.game_active:
             ship.update()
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
             gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
 
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
 
-        # Get rid of bullets that have disappeared.
+        # Get rid of bullets that have disappeared.f
         for bullet in bullets.copy():
             if bullet.rect.bottom <= 0:
                 bullets.remove(bullet)
         print(len(bullets))
 
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
         # Watch for keyboard and mouse events.
         #  ffoghu
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
+        # Draw the play button if the game is inactive.
+        if not stats.game_active:
+            play_button.draw_button()
+
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
 
 run_game()
