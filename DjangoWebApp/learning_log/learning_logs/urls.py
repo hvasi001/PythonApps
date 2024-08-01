75db7916ed50b98b15dfe181a510b6850ae70ea1
hvasi001 <haripriya.vasireddy@gmail.com>
2024-07-31 18:45:48 -0700

    hari

--- DjangoWebApp/learning_log/learning_logs/urls.py
+++ DjangoWebApp/learning_log/learning_logs/urls.py
@@ -12,4 +12,7 @@ urlpatterns = [
     # Page for adding a new topic
     path('new_topic/', views.new_topic, name='new_topic'),
     path('topics/', views.topics, name='topics'),
+
+    # Detail page for a single topic
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
